408143a1f5dc31dc883376bfb03673857e162547
r a f t <hakan.eryargi@gmail.com>
2020-04-15 15:11:57 +0200

    Changes for extending a cross-cluster network

--- fabric-kube/extend.sh
+++ fabric-kube/extend.sh
@@ -20,6 +20,16 @@ cd $project_folder
 echo "-- extending certificates --"
 cryptogen extend --config ./crypto-config.yaml --input crypto-config
 
+# place holder empty folders for external peer orgs
+externalPeerOrgs=$(yq '.ExternalPeerOrgs // empty' ./crypto-config.yaml -r -c)
+if [ "$externalPeerOrgs" ]; then
+    echo "-- creating empty folders for external peer orgs --"
+    for peerOrgDomain in $(echo "$externalPeerOrgs" | jq -r '.[].Domain'); do
+        echo "$peerOrgDomain"
+        mkdir -p "./crypto-config/peerOrganizations/$peerOrgDomain/msp"
+    done
+fi 
+
 # copy stuff hlf-kube folder (as helm charts cannot access files outside of chart folder)
 # see https://github.com/helm/helm/issues/3276#issuecomment-479117753
 cd $current_folder
